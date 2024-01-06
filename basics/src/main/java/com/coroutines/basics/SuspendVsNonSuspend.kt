205293b413f1015404a203b09aaceacc8e6eebef
n.dipir <nargesdeypir@gmail.com>
2024-01-06 08:53:48 +0330

    BASIC MODULE -- thread callback with bytecode

--- basics/src/main/java/com/coroutines/basics/SuspendVsNonSuspend.kt
+++ basics/src/main/java/com/coroutines/basics/SuspendVsNonSuspend.kt
@@ -1,11 +1,30 @@
 package com.coroutines.basics
 
+import kotlin.concurrent.thread
+
 fun main() {
+    getUserFromNetworkCallback("202") { user ->
+        println(user)
+    }
+    println("main end")
 }
 
 fun getUserStandard(userId: String): User {
     Thread.sleep(1000)
     return User(userId, "Filip")
+}
+
+fun getUserFromNetworkCallback(
+    userId: String,
+    onUserReady: (User) -> Unit,
+) {
+    thread {
+        Thread.sleep(1000)
+
+        val user = User(userId, "Filip")
+        onUserReady(user)
+    }
+    println("end")
 }
 
 data class User(
