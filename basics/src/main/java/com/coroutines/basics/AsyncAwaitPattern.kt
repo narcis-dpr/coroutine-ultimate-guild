80ee42c605b8fafe17a8fb069153a1755689c232
n.dipir <nargesdeypir@gmail.com>
2024-01-07 11:09:08 +0330

    BASIC MODULE -- use two deferred values in a single function call

--- basics/src/main/java/com/coroutines/basics/AsyncAwaitPattern.kt
+++ basics/src/main/java/com/coroutines/basics/AsyncAwaitPattern.kt
@@ -2,7 +2,9 @@ package com.coroutines.basics
 
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.io.File
 
 fun main() {
     val userId = 992
@@ -14,9 +16,28 @@ fun main() {
      println(user)
      }
      */
+    /**
+     *
+     Async/Await pattern
+     GlobalScope.launch {
+     val userData = getUserByIdFromNetwork(userId)
+     println(userData.await())
+     }
+     Thread.sleep(5000)
+     */
     GlobalScope.launch {
-        val userData = getUserByIdFromNetwork(userId)
-        println(userData.await())
+        println("Finding user")
+        val userDeferred = getUserByIdFromNetwork(userId) // deferred number one
+        val usersFromFileDeferred = readUsersFromFile("basics/users.txt") // deferred number two
+
+        val userStoredInFile = checkUserExists(
+            userDeferred.await(),
+            usersFromFileDeferred.await(),
+        ) // combining two deferred
+
+        if (userStoredInFile) {
+            println("Found user in file")
+        }
     }
     Thread.sleep(5000)
 }
@@ -25,12 +46,42 @@ private fun getUserByIdFromNetworkThread(userId: Int, onUserReady: (AsyncUser) -
     Thread.sleep(3000)
     onUserReady(AsyncUser(userId, "Filip", "Babic"))
 }
+
 private fun getUserByIdFromNetwork(userId: Int) =
     GlobalScope.async {
         println("Retrieving user from network")
         Thread.sleep(3000)
         AsyncUser(userId, "Filip", "Babic")
     }
+
+private fun readUsersFromFile(filePath: String) = GlobalScope.async {
+    println("Reading the file of users")
+    delay(1000)
+
+    File(filePath)
+        .readLines()
+        .asSequence()
+        .filter { it.isNotEmpty() }
+        .map {
+            val data = it.split(" ") // makes it : [id, name, lastName]
+
+            if (data.size == 3) data else emptyList()
+        }
+        .filter { it.isNotEmpty() }
+        .map {
+            val userId = it[0].toInt()
+            val name = it[1]
+            val lastName = it[2]
+
+            AsyncUser(userId, name, lastName)
+        }
+        .toList()
+}
+
+private fun checkUserExists(user: AsyncUser, users: List<AsyncUser>):
+    Boolean {
+    return user in users
+}
 
 internal data class AsyncUser(
     val userId: Int,
