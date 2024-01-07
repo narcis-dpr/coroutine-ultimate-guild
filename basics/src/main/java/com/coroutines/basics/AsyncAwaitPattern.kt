f12da43f36938fe27572907d1b078bff1c6f77bf
n.dipir <nargesdeypir@gmail.com>
2024-01-07 08:28:52 +0330

    BASIC MODULE -- replace thread with async/await

--- basics/src/main/java/com/coroutines/basics/AsyncAwaitPattern.kt
+++ basics/src/main/java/com/coroutines/basics/AsyncAwaitPattern.kt
@@ -1,16 +1,28 @@
 package com.coroutines.basics
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
 fun main() {
     val userId = 992
-    getUserByIdFromNetwork(userId) { user ->
-        println(user)
+    GlobalScope.launch {
+        val userData = getUserByIdFromNetwork(userId)
+        println(userData.await())
     }
+    Thread.sleep(5000)
 }
 
-private fun getUserByIdFromNetwork(userId: Int, onUserReady: (AsyncUser) -> Unit) {
+private fun getUserByIdFromNetworkThread(userId: Int, onUserReady: (AsyncUser) -> Unit) {
     Thread.sleep(3000)
     onUserReady(AsyncUser(userId, "Filip", "Babic"))
 }
+private fun getUserByIdFromNetwork(userId: Int) =
+    GlobalScope.async {
+        println("Retrieving user from network")
+        Thread.sleep(3000)
+        AsyncUser(userId, "Filip", "Babic")
+    }
 
 internal data class AsyncUser(
     val userId: Int,
