b963df2193b565fd15f0c480b52e77ec9c6672ef
n.dipir <nargesdeypir@gmail.com>
2024-01-08 15:50:27 +0330

    BASIC MODULE -- work-stealing executor for custom context of a coroutine

--- basics/src/main/java/com/coroutines/basics/contextSwitch/ContextSwitch.kt
+++ basics/src/main/java/com/coroutines/basics/contextSwitch/ContextSwitch.kt
@@ -2,7 +2,9 @@ package com.coroutines.basics.contextSwitch
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
 
 fun main() {
     GlobalScope.launch { // emptyCoroutineContext
@@ -22,7 +24,18 @@ fun main() {
     Thread.sleep(50)
 
     GlobalScope.launch(context = Dispatchers.Unconfined) {
+        // which dispatcher is Unconfined running on?
         println(Thread.currentThread().name)
     }
+    Thread.sleep(50)
+
+    // creating a new work-stealing executor for executing a sample task :
+
+    val executorDispatcher = Executors.newWorkStealingPool().asCoroutineDispatcher()
+
+    GlobalScope.launch(context = executorDispatcher) {
+        println(Thread.currentThread().name)
+    }
+
     Thread.sleep(50)
 }
