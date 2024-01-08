cb104b689cb64b93c739b27c7e7619a60cf05c68
n.dipir <nargesdeypir@gmail.com>
2024-01-08 15:38:15 +0330

    BASIC MODULE -- print the name of dispatcher

--- basics/src/main/java/com/coroutines/basics/contextSwitch/ContextSwitch.kt
+++ basics/src/main/java/com/coroutines/basics/contextSwitch/ContextSwitch.kt
@@ -4,8 +4,13 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 fun main() {
-    GlobalScope.launch {
+    GlobalScope.launch { // emptyCoroutineContext
         println("This is a coroutine")
+    }
+    Thread.sleep(50)
+
+    GlobalScope.launch {  // print the name of current thread aka DefaultDispatcher
+        println(Thread.currentThread().name)
     }
     Thread.sleep(50)
 }
