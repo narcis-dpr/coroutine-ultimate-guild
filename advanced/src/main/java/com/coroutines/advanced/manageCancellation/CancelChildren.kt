8d94b576ac1ea00da03f8942f74307e1da3fc793
n.dipir <nargesdeypir@gmail.com>
2024-01-10 10:18:38 +0330

    ADVANCED MODULE -- cancel children and handle their cancellation

---
+++ advanced/src/main/java/com/coroutines/advanced/manageCancellation/CancelChildren.kt
@@ -0,0 +1,44 @@
+package com.coroutines.advanced.manageCancellation
+
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val parentJob = launch {
+        val childOne = launch {
+            repeat(1000) { i ->
+                println(
+                    "Child Coroutine 1: " +
+                        "$i. Crunching numbers [Beep.Boop.Beep]...",
+                )
+
+                delay(500L)
+            }
+        }
+
+        // Handle the exception thrown from `launch`
+        // coroutine builder
+        childOne.invokeOnCompletion { exception ->
+            println("Child One: ${exception?.message}")
+        }
+
+        val childTwo = launch {
+            repeat(1000) { i ->
+                println("Child coroutine 2: $i. Crunching numbers [Beep.Boop.Beep]...")
+                delay(500L)
+            }
+        }
+        // Handle the exception thrown from `launch`
+        // coroutine builder
+        childTwo.invokeOnCompletion { exception ->
+            println("child two: ${exception?.message}")
+        }
+    }
+    delay(1200L)
+
+    println("Calling cancelChildren() on the parentJob")
+    parentJob.cancelChildren()
+    println("parentJob isActive ${parentJob.isActive}")
+}
