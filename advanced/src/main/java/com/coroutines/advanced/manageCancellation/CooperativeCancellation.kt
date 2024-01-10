4c0ddc1cfff7b1f0fa67ba6b0f5d6cc8eddd75a8
n.dipir <nargesdeypir@gmail.com>
2024-01-10 08:12:50 +0330

    ADVANCED MODULE -- wrong way of cancelling a coroutine (i doesn't cancel at all !)

---
+++ advanced/src/main/java/com/coroutines/advanced/manageCancellation/CooperativeCancellation.kt
@@ -0,0 +1,25 @@
+package com.coroutines.advanced.manageCancellation
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val job = launch(Dispatchers.Default) {
+        var nextPrintTime = startTime
+        var i = 0
+        while (i < 10) {
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("Doing heavy work $i")
+                i++
+                nextPrintTime += 500L
+            }
+        }
+    }
+    delay(1000)
+    println("Cancelling coroutine")
+    job.cancel()
+    println("Main: now I can quit!")
+}
