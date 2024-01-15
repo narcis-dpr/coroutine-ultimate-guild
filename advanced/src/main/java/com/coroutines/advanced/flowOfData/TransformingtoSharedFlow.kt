af8efdba7e716839ffb6329e9831d767200ab483
n.dipir <nargesdeypir@gmail.com>
2024-01-15 08:29:39 +0330

    ADVANCED MODULE -- flow: hot flow; transform a regular flow to shared flow

---
+++ advanced/src/main/java/com/coroutines/advanced/flowOfData/TransformingtoSharedFlow.kt
@@ -0,0 +1,32 @@
+package com.coroutines.advanced.flowOfData
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.isActive
+
+fun main() {
+    val coroutineScope = CoroutineScope(Dispatchers.Default)
+    val sharedFlow = flow {
+        emit(5)
+        emit(3)
+        emit(1)
+        Thread.sleep(50)
+        coroutineScope.cancel()
+    }.shareIn(coroutineScope, started = SharingStarted.Lazily)
+
+    sharedFlow.onEach {
+        println("Emitting $it")
+    }.launchIn(coroutineScope)
+
+    while (coroutineScope.isActive) {
+    }
+}
+/**
+ * using sharedIn we transform a flow into a shredFlow
+ */
