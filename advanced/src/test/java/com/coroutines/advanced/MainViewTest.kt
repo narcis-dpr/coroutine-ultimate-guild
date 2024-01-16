7958113d2a9b4e0d45e1b7ff3340ac4c8c9ca354
n.dipir <nargesdeypir@gmail.com>
2024-01-16 08:26:21 +0330

    ADVANCED MODULE -- coroutine testing: mainView: sample runTest block

--- advanced/src/test/java/com/coroutines/advanced/MainViewTest.kt
+++ advanced/src/test/java/com/coroutines/advanced/MainViewTest.kt
@@ -4,22 +4,35 @@ import com.coroutines.advanced.coroutineTest.presentation.MainPresenter
 import com.coroutines.advanced.coroutineTest.view.MainView
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNull
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
-
 
 class MainViewTest {
     private val mainPresenter by lazy { MainPresenter() }
     private val mainView by lazy { MainView(mainPresenter) }
 
     @Test
-    fun testFetchUserData() {
+    fun testFetchUserData() = runTest {
         // arrange: initial state
         assertNull(mainView.userData)
-        //act: updating the state
+        // act: updating the state
         mainView.fetchUserData()
         // assert: checking the new state, and printing it out
         assertEquals("Filip", mainView.userData?.name)
         mainView.printUserData()
+    }
 
+    @Test
+    fun exampleTest() = runTest {
+        // acts like runBlocking but instead of actually delaying the time, it advances the test time
+        val deferred = async {
+            delay(1_000)
+            async {
+                delay(1_000)
+            }.await()
+        }
+        deferred.await()
     }
 }
