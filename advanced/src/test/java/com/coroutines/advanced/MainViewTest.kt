5234ae2c142ec38e85433ff2396fd099de8ba2ab
n.dipir <nargesdeypir@gmail.com>
2024-01-16 09:04:17 +0330

    ADVANCED MODULE -- coroutine testing: mainView: advance time to move pass delay and make test run

--- advanced/src/test/java/com/coroutines/advanced/MainViewTest.kt
+++ advanced/src/test/java/com/coroutines/advanced/MainViewTest.kt
@@ -1,15 +1,16 @@
 package com.coroutines.advanced
 
-import com.coroutines.advanced.coroutineTest.contextProvider.CoroutineContextProvider
 import com.coroutines.advanced.coroutineTest.contextProvider.CoroutineContextProviderImpl
 import com.coroutines.advanced.coroutineTest.presentation.MainPresenter
 import com.coroutines.advanced.coroutineTest.view.MainView
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -22,12 +23,17 @@ class MainViewTest {
 
     private val mainPresenter by lazy { MainPresenter() }
     private val mainView by lazy { MainView(mainPresenter, testCoroutineContextProvider, testCoroutineScope) }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun testFetchUserData(): Unit = testCoroutineScope.runTest {
         // arrange: initial state
         assertNull(mainView.userData)
         // act: updating the state
         mainView.fetchUserData()
+        // advance the time to make test pass :
+        // advanceTimeBy(1010) : this works and advanceUntilIdle works just fine too!
+        advanceUntilIdle()
         // assert: checking the new state, and printing it out
         assertEquals("Filip", mainView.userData?.name)
         mainView.printUserData()
