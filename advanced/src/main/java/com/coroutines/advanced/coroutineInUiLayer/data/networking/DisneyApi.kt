6113c406b57384a5a29bf160edda124d2aaa124e
n.dipir <nargesdeypir@gmail.com>
2024-01-20 09:24:44 +0330

    ADVANCED MODULE -- network layer: make api call suspend and remove Call return type

--- advanced/src/main/java/com/coroutines/advanced/coroutineInUiLayer/data/networking/DisneyApi.kt
+++ advanced/src/main/java/com/coroutines/advanced/coroutineInUiLayer/data/networking/DisneyApi.kt
@@ -2,11 +2,10 @@
 package com.coroutines.advanced.coroutineInUiLayer.data.networking
 
 import com.raywenderlich.android.disneyexplorer.data.model.CharactersResponse
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface DisneyApi {
 
     @GET("characters")
-    fun getCharacters(): Call<CharactersResponse>
+    suspend fun getCharacters(): CharactersResponse
 }
--- advanced/src/main/java/com/coroutines/advanced/coroutineInUiLayer/data/networking/DisneyApiService.kt
+++ advanced/src/main/java/com/coroutines/advanced/coroutineInUiLayer/data/networking/DisneyApiService.kt
@@ -2,31 +2,11 @@
 package com.coroutines.advanced.coroutineInUiLayer.data.networking
 
 import com.raywenderlich.android.disneyexplorer.data.model.CharactersResponse
-import com.raywenderlich.android.disneyexplorer.data.model.DisneyCharacter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class DisneyApiService(private val disneyApi: DisneyApi) {
 
     suspend fun getCharacters(): Result<CharactersResponse> =
-        withContext(Dispatchers.IO) {
-            try {
-                val data = disneyApi.getCharacters().execute().body()
-                if (data == null) {
-                    Result.failure(Throwable("No response"))
-                } else {
-                    Result.success(data)
-                }
-            } catch (error: Throwable) {
-                Result.failure(error)
-            }
+        kotlin.runCatching {
+            disneyApi.getCharacters()
         }
-
-    fun getDisneyCharacters(): List<DisneyCharacter> {
-        // Initiate the request
-        val response = disneyApi.getCharacters().execute()
-
-        // Process the response
-        return response.body()?.data ?: emptyList()
-    }
 }
